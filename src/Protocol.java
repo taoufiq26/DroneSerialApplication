df161b3758a91c0a6f3b3711c914731fe31b8bf6
EL FAKIR YAHYA <yahya.elfakir@gmail.com>
2014-12-31 15:07:44 +0000

    Cleaning the project

--- src/Protocol.java
+++
@@ -1,8 +0,0 @@
-public interface Protocol {  
-      
-    // protocol manager handles each received byte  
-    void onReceive(byte b);  
-      
-    // protocol manager handles broken stream  
-    void onStreamClosed();  
-}  
